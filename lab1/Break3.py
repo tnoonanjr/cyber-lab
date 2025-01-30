9c3297e94a2c0f8e675ca96a0894ebc73b0314c3
Thomas Noonan <146210092+tnoonanjr@users.noreply.github.com>
2025-01-30 11:14:53 -0500

    Update Break3.py

--- lab1/Break3.py
+++ lab1/Break3.py
@@ -11,9 +11,11 @@ key = SkeletonKey(passwd_file_path = test_param[0],
                     cracked_users=set(["SkyRedFalcon914", "MontainBlueFalcon157"]))
 
 start = time()
-key.crack_pass_bruteforce(max_cracked=1, print_interval=10000)
+cracked_users = key.crack_pass_bruteforce(max_cracked=1, print_interval=10000)
 end = time()
 
 print(f"""Start: {start}
 End: {end}
-Elapsed: {end-start}""")
+Elapsed: {end-start}
+=========- Users Cracked -=========
+{cracked_users}""")
