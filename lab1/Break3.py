37b9eacbcb1e18f3111e7c6f71cff2503c22e3cb
clf20001 <cooper.frank@uconn.edu>
2025-02-02 22:39:16 -0500

    Update Break3.py

--- lab1/Break3.py
+++ lab1/Break3.py
@@ -1,5 +1,9 @@
 from SkeletonKey import SkeletonKey
-from time import time                                                                                                                                                                                                                                                                                                            
+from time import time
+from datetime import datetime
+
+start_float = time()
+start_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")                                                                                                                                                                                                                                                                                                            
 
 test_param = ["/home/cse/Lab1/Q3/PwnedPWs100k",   # passwd path
               "/home/cse/Lab1/Q3/gang",            # user path
@@ -8,14 +12,14 @@ test_param = ["/home/cse/Lab1/Q3/PwnedPWs100k",   # passwd path
 key = SkeletonKey(passwd_file_path = test_param[0],
                   user_file_path = test_param[1],
                   exe_path = test_param[2],
-                    cracked_users=set(["SkyRedFalcon914", "MontainBlueFalcon157"]))
+                  cracked_users={"SkyRedFalcon914", "MountainPurpleShark585"},
+                  print_interval=1000)
 
-start = time()
-cracked_users = key.crack_pass_bruteforce(max_cracked=1, print_interval=10000)
-end = time()
+# Crack password
+key.crack_pass_bruteforce(max_cracked=1)
 
-print(f"""Start: {start}
-End: {end}
-Elapsed: {end-start}
-=========- Users Cracked -=========
-{cracked_users}""")
+end_float = time()
+end_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+elapsed = end_float - start_float
+
+print(f"Start time: [{start_str}] \nEnd time: [{end_str}] \nElapsed time: {round(elapsed, 2)} sec")
