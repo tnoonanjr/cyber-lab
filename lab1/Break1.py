e434f38a3b403bce82e6d72ff067ed1537bc9ac2
clf20001 <cooper.frank@uconn.edu>
2025-02-02 22:38:45 -0500

    Update Break1.py

--- lab1/Break1.py
+++ lab1/Break1.py
@@ -1,5 +1,9 @@
 from SkeletonKey import SkeletonKey
 from time import time
+from datetime import datetime
+
+start_float = time()
+start_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 params = ["/home/cse/Lab1/Q1/MostCommonPWs",   # passwd path
           "SkyRedFalcon914",                   # users
@@ -7,14 +11,14 @@ params = ["/home/cse/Lab1/Q1/MostCommonPWs",   # passwd path
         ]
 key = SkeletonKey(passwd_file_path = params[0],
                   users = params[1],
-                  exe_path = params[2])
+                  exe_path = params[2],
+                  print_interval=1)
 
-start = time()
-key.crack_pass_bruteforce()
-end = time()
+# Crack password
+key.crack_pass_bruteforce(max_cracked=1)
 
-print(f"""Start: {start}
-End: {end}
-Elapsed: {end-start}""")
+end_float = time()
+end_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+elapsed = end_float - start_float
 
-# expected correct: 1234567890
+print(f"Start time: [{start_str}] \nEnd time: [{end_str}] \nElapsed time: {round(elapsed, 2)} sec")
