35b4e47d23a83018e1cd9be73805965b6ee3d3d1
Thomas Noonan <146210092+tnoonanjr@users.noreply.github.com>
2025-02-10 16:14:48 -0500

    Update Break3.py

--- lab1/Break3.py
+++ lab1/Break3.py
@@ -9,11 +9,11 @@ test_param = ["/home/cse/Lab1/Q3/PwnedPWs100k",   # passwd path
               "/home/cse/Lab1/Q3/gang",            # user path
               "/home/cse/Lab1/Q3/Login.pyc"        # exe path
             ]
-key = SkeletonKey(passwd_file_path = test_param[0],
+key = SkeletonKey(passwd_pwned_path = test_param[0],
                   user_file_path = test_param[1],
                   exe_path = test_param[2],
-                  cracked_users={"SkyRedFalcon914", "MountainPurpleShark585"},
-                  print_interval=1000)
+                  cracked_users=set(["SkyRedFalcon914", "MontainBlueFalcon157"]), 
+                  print_interval=10000)
 
 # Crack password
 key.crack_pass_bruteforce(max_cracked=1)
