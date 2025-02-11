11905583832393a75a3ba8662600bb294e3767f3
clf20001 <cooper.frank@uconn.edu>
2025-02-10 22:31:10 -0500

    Update Break1.py

--- lab1/Break1.py
+++ lab1/Break1.py
@@ -5,17 +5,13 @@ from datetime import datetime
 start_float = time()
 start_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-params = ["/home/cse/Lab1/Q1/MostCommonPWs",   # passwd path
-          "SkyRedFalcon914",                   # users
-          "/home/cse/Lab1/Q1/Login.pyc"        # exe path
-        ]
-key = SkeletonKey(passwd_file_path = params[0],
-                  users = params[1],
-                  exe_path = params[2],
-                  print_interval=1)
+key = SkeletonKey(passwd_file_path = "/home/cse/Lab1/Q1/MostCommonPWs",
+                  users = "SkyRedFalcon914",
+                  login_file_path = "/home/cse/Lab1/Q1/Login.pyc",
+                  print_interval = 1)
 
 # Crack password
-key.crack_pass_bruteforce(max_cracked=1)
+key.crack_pass_bruteforce()
 
 end_float = time()
 end_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
