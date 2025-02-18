a3c689fd5596fadc4a87102991ac4e48813ab7a6
Thomas J. Noonan <tjn4365@gmail.com>
2025-02-17 19:45:56 -0500

    Create skeleton code for Q2worm

---
+++ lab2/Solutions/Q2worm.py
@@ -0,0 +1,31 @@
+import socket
+import paramiko
+import telnetlib
+
+def find_vulnerable_machines():
+    '''
+    Output: 
+    open_ssh.log or open_telnet.log
+    - File of ips with open ports using either ssh or telnet.
+
+    '''
+
+    for ip_last_octet in range(256):
+        pass
+
+def find_vulnerable_users():
+    '''
+    Output: 
+    ssh_accounts.log
+    telnet_accounts.log
+    - File in ip,user,passwd format of cracked users.
+
+    '''
+    pass
+
+def extract_and_infect():
+    '''
+    
+    
+    '''
+    pass
