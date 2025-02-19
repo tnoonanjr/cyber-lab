bac52124f529aeb901eb72a3f045f6c07a5db43a
Thomas Noonan <146210092+tnoonanjr@users.noreply.github.com>
2025-02-19 21:40:20 +0000

    Update try_ports

--- lab2/Solutions/try_ports.sh
+++ lab2/Solutions/try_ports.sh
@@ -6,19 +6,19 @@ echo "ValueError: missing 1 argument"
 echo "Usage: ./try_ports.sh [ip address]"
 else
 
-    log_ssh = "open_ssh.log"
-    log_telnet = "open_telnet.log"
-    > "$log_ssh"
-    > "$log_telnet"
+    log_ssh="open_ssh.log"
+    log_telnet="open_telnet.log"
+    >"$log_ssh"
+    >"$log_telnet"
 
     for last_byte in $(seq 1 254); do
     # Test the port and print the line if it connects
-        if  nc -zv -w 1 $1.$last_byte 22 then
-            echo "$1.$last_byte\n" >> "$log_ssh"
+        if  nc -zv -w 1 $1.$last_byte 22 2>dev/null; then
+            echo "$1.$last_byte" >> "$log_ssh"
         fi
 
-        if nc -zv -w 1 $1.$last_byte 23 then
-            echo "$1.$last_byte\n" >> "$log_telnet"
+        if nc -zv -w 1 $1.$last_byte 23 2>dev/null; then
+            echo "$1.$last_byte" >> "$log_telnet"
         fi
     
     done
