b9a78c0ae0247dacaba832481eb2ba32a0dc9d9d
Christopher Rauch <153033298+3cr3@users.noreply.github.com>
2025-02-19 16:01:08 -0500

    try_ports update

--- lab2/Solutions/try_ports.sh
+++ lab2/Solutions/try_ports.sh
@@ -6,9 +6,8 @@ echo "ValueError: missing 1 argument"
 echo "Usage: ./try_ports.sh [ip address]"
 
 else
-for byte in `seq 1 254`; do
+for port in `seq 1 254`; do
 # Test the port and print the line if it connects
-#ssh -o ConnectTimeout=1 $1"."$byte # | grep -h "Connected to" &
-telnet -w 1 $1"."$byte
+nc -zv -w 1 $1 $port 2>&1 | grep succeeded &
 done
 fi
