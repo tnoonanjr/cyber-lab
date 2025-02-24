866d4efe3b5d8ce303eab2cf74ff442d26570586
Thomas Noonan <146210092+tnoonanjr@users.noreply.github.com>
2025-02-24 00:29:36 +0000

    Update tryacc

--- lab2/Solutions/try_accounts.sh
+++ lab2/Solutions/try_accounts.sh
@@ -3,12 +3,12 @@
 if [ -z == $1 || -z == $2 || -z == $3 || -z == $4] then
 echo "ValueError: missing an argument (expected 2)"
 echo "Usage: ./try_accounts.sh ip user passwd protocol(ssh=0/telnet=1)"
+fi
 
 ip=$1
 user=$2
 passwd=$3
 protocol=$4
-else
 if [ "$protocol" == "0" ] then
     sshpass -p "$passwd" "$user"@"$ip";
 elif [ "$protocol" == "0" ] then
@@ -16,4 +16,6 @@ elif [ "$protocol" == "0" ] then
         echo "$user"
         echo "$passwd"
     } | telnet $ip
+else
+    echo "Unsupported protocol: must be 0 (ssh) or 1 (telnet)"
 fi
