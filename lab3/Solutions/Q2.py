c0d34552e3264ec7589be70f41a415bad2305cae
tmaceira <thomas.maceira@uconn.edu>
2025-03-05 15:55:39 -0500

    Update Q2.py

--- lab3/Solutions/Q2.py
+++ lab3/Solutions/Q2.py
@@ -6,27 +6,24 @@ def find_hash():
     output = subprocess.run(["ls", "../Q2files"], capture_output=True, text=True).stdout.strip("\n")
     
     files = output.split()
-    
-    with open("../Q2hash.txt", "rb") as f:
-        bin_text = f.read()
         
     with open("../Q2hash.txt", "r") as g:
         base_text = g.read()
         
-    Q2_h = SHA256.new(bin_text)
-    Q2_hash = Q2_h.hexdigest()
     print(base_text)
-    print(Q2_hash)
     
     for file in files:
-        bin_file = file.encode()
+        with open("../Q2files/" + file, "rb") as bt:
+            
+            bin_file = bt.read()
+        
         h = SHA256.new(bin_file)
         hashed_file = h.hexdigest()
         print(hashed_file)
         
-        if Q2_hash == hashed_file:
-            print("Done!")
-    print("Completed")
+        if base_text == hashed_file:
+            print("Found!")
+            break
     
 if __name__ == "__main__":
     find_hash()
