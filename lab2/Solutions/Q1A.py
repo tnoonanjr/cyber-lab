617355ae36db2746bb69f08e2f6b30e974fdd911
tmaceira <thomas.maceira@uconn.edu>
2025-03-05 15:40:50 -0500

    Delete lab2 directory

--- lab2/Solutions/Q1A.py
+++
@@ -1,6 +0,0 @@
-from Parasite import Parasite
-import os
-
-current_directory = os.path.dirname(os.path.realpath(__file__))
-
-pwd_py_files = Parasite(output_path=os.path.join(current_directory, "Q1A.out")).getPyFiles()
--- lab2/Solutions/Q1C.py
+++
@@ -1,3 +0,0 @@
-from Parasite import Parasite
-
-Parasite(output_path="Q1C.out", payload_file="Q1C_payload.py").inject_all()
