0ec927b1576ee378dd4deb4eca98541c85f15a0e
Thomas Noonan <146210092+tnoonanjr@users.noreply.github.com>
2025-03-05 06:09:50 -0500

    Update Q1.py

--- lab3/Solutions/Q1.py
+++ lab3/Solutions/Q1.py
@@ -1,11 +1,21 @@
 import subprocess
+import os
 
-def Q1(Q1_file_path):
+def get_exe_files(path):
+    exe_files = []
+    directory = os.listdir(path)
+    for file in directory:
+        file_path = f"{path}/{file}"
+        if os.path.isfile(file_path) and file_path.endswith(".exe"):
+            exe_files.append(file_path.split("/")[-1])
+    return exe_files
+
+def bruteforce_find_checksum_match(Q1_file_path):
     exe_file_queue = get_exe_files(Q1_file_path)
     for file in exe_file_queue:    
-        exe_path = ""
+        exe_path = "lab3/Q1files"
         run_message = subprocess.run([f"sha256sum {exe_path}"], capture_output=True, text=True)
         print(run_message)
 
 Q1_file_path = "lab3/Q1hash.txt"
-Q1(Q1_file_path)
+bruteforce_find_checksum_match(Q1_file_path)
