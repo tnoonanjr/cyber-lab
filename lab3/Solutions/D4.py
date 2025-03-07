c5537c6363464b13bdeeb6fb47408d7791e64fb9
tmaceira <thomas.maceira@uconn.edu>
2025-03-06 19:24:59 -0500

    Create D4.py

---
+++ lab3/Solutions/D4.py
@@ -0,0 +1,31 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+
+
+def D4():
+    
+    with open("../Q4files/Encrypted4", "rb") as f:
+        f.scan(16)
+        encrypted_text = f.read()
+    
+    with open("../Q4files/.key.txt", "rb") as g:
+        variable = g.read()
+    
+    cipher = AES.new(variable, AES.MODE_CBC)
+    
+    
+    plaintext = cipher.decrypt(encrypted_text)
+    
+    
+    unpadded = unpad((plaintext), AES.block_size)
+    
+    print(unpadded)
+
+    # Try decoding with a common encoding, like 'utf-8', 'latin1', or 'iso-8859-1'
+    decoded_string = unpadded.decode('latin1', errors='ignore')  # Using 'latin1' for this example
+    print(decoded_string)
+    
+    
+
+if __name__ == "__main__":
+    D4()
